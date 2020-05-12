4f702351e2ca9e9f059010452ffc772547ba76d2
Tram13 <arno.vermote@hotmail.com>
2020-05-13 00:56:51 +0200

    First real commit, will now statically pass images H A P P Y  D A Y S

--- express/app.js
+++ express/app.js
@@ -1,20 +1,65 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Dependencies
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const createError = require('http-errors');
+const indexRouter = require('./routes/index');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Creating express instance
+const app = express();
 
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// Setting up middleware
+app.use(logger('dev', {}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
+
+// Whitelist of static folder
+app.use('/public', (req, res, next) => {
+    // Will only allow files in /public/images/ ** name in whitelistedImages **
+    const whitelistedImages = "friendship.png|uwma.png"
+    const regex = new RegExp("^\/images\/(" + whitelistedImages + ")$")
+    const isWhitelisted = req.url.match(regex)
+    if (!isWhitelisted) {
+        return res.status(403).end('403 Forbidden')
+    }
+    next()
+})
+// Setting alias for /public/images
+app.use('/static_images', express.static(path.join(__dirname, 'public', 'images')));
+
+// Setting Main Router
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+// Redirect all requests with trailing '/' to one without
+app.use((req, res, next) => {
+    const test = /\?[^]*\//.test(req.url);
+    if (req.url.substr(-1) === '/' && req.url.length > 1 && !test)
+        res.redirect(301, req.url.slice(0, -1));
+    else
+        next();
+});
+
+// CATCH 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// Error Handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.format({
+        'application/json': function () {
+            res.json({error: 'Not found'});
+        }
+    });
+});
 
 module.exports = app;
--- express/routes/index.js
+++ express/routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.sendFile(path.join(__dirname, '../public', 'images', 'friendship.png'), function(err) {
+        if (err) {
+            console.log(err)
+            next(err)
+        }
+    })
 });
 
 module.exports = router;
