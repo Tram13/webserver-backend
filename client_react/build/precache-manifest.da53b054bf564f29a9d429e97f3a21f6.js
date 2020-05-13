7c42b6ff82110e9bcddbbcbb0bb2c9f4c41ada1b
Tram13 <arno.vermote@hotmail.com>
2020-05-13 20:49:14 +0200

    added build files for react

---
+++ client_react/build/precache-manifest.da53b054bf564f29a9d429e97f3a21f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ccd2b476f886da131ae5a66c26a067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed72dacfeb8d9dbd1875",
+    "url": "/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "c05e46d60bf8efe0692f",
+    "url": "/static/css/main.4191e99a.chunk.css"
+  },
+  {
+    "revision": "ed72dacfeb8d9dbd1875",
+    "url": "/static/js/2.9f762153.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.9f762153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c05e46d60bf8efe0692f",
+    "url": "/static/js/main.ca6fd67d.chunk.js"
+  },
+  {
+    "revision": "69ebdab3fb0660e0ce6b",
+    "url": "/static/js/runtime-main.aaccf681.js"
+  }
+]);
