e24f2b0cb9d71c5871337a4afcb2609db422d3a0
Tram13 <arno.vermote@hotmail.com>
2020-05-13 15:28:02 +0200

    kan werken, miss kan het ook beter

--- client_react/src/Components/Content/Wout.js
+++ client_react/src/Components/Content/Wout.js
@@ -28,9 +28,22 @@ class Wout extends React.Component {
     }
 
     render() {
-        const kop1 = <img className="materialboxed" width="650" src={this.state.data["kop1"]} alt="Kop1"/>
-        const kop2 = <img className="materialboxed" width="650" src={this.state.data["kop2"]} alt="Kop2"/>
-        const kop3 = <img className="materialboxed" width="650" src={this.state.data["kop3"]} alt="Kop3"/>
+        const kop1 = <img className="materialboxed responsive-img" width="650" src={this.state.data["kop1"]}
+                          alt="Kop 1"/>
+        const kop2 = <img className="materialboxed responsive-img" width="650" src={this.state.data["kop2"]}
+                          alt="Kop 2"/>
+        const kop3 = <img className="materialboxed responsive-img" width="650" src={this.state.data["kop3"]}
+                          alt="Kop 3"/>
+        const panel1 = <div>
+            <div className="row">
+                <div className="column">
+                    {kop1}
+                </div>
+                <div className="column">
+                    {kop2}
+                </div>
+            </div>
+        </div>
         if (this.state.fetching) {
             return (
                 <LoadingAnimation/>
@@ -38,9 +51,17 @@ class Wout extends React.Component {
         } else {
             return (
                 <div>
-                    {kop1}
-                    {kop2}
-                    {kop3}
+                    <h3>Wout zuigt ballen in WebDev</h3>
+                    <div>
+                        <h5>when you sudo reboot and it doesn't work anymore</h5>
+                        <div>
+                            {panel1}
+                        </div>
+                    </div>
+                    <div>
+                        <h5>when you do it again</h5>
+                        {kop3}
+                    </div>
                 </div>
             )
         }
