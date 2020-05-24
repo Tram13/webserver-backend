b48f9538f6caa83191a209d4eb3b822677eed7aa
Tram13 <arno.vermote@hotmail.com>
2020-05-25 00:15:02 +0200

    updated routes

--- server_express/routes/suggestions.js
+++ server_express/routes/suggestions.js
@@ -2,11 +2,11 @@ const express = require('express');
 const SuggestionsController = require('../controllers/SuggestionsController');
 const router = express.Router();
 
-// suggestions/create
-router.post('/create', SuggestionsController.createSuggestionPost);
-
 // suggestions/:suggestionID/delete
 router.delete('/:suggestionID/delete', SuggestionsController.deleteSuggestionDelete);
+
+// suggestions/create
+router.post('/', SuggestionsController.createSuggestionPost);
 
 // suggestions
 router.get('/', SuggestionsController.suggestionsList);
