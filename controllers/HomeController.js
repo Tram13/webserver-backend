814a10d694f2b3e02dffa45dfa8afdb575719b54
Tram13 <arno.vermote@hotmail.com>
2020-06-07 15:29:20 +0200

    updated to NGINX static serve

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,6 +1,6 @@
 exports.imagesList = function(req, res) {
     const index = "https://api.tram13.me";
-    const baseUrl = index + "/static_images/home/";
+    const baseUrl = index + "/public/images/home/";
     res.status(200).json({
         index: index,
         baseUrl : baseUrl,
