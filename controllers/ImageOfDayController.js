1e13f48cfdea244a0cc65c22ea066f1faeab0b92
Tram13 <arno.vermote@hotmail.com>
2020-06-18 15:20:53 +0200

    fixed errors, now it should give the correct url

--- controllers/ImageOfDayController.js
+++ controllers/ImageOfDayController.js
@@ -5,15 +5,24 @@ exports.imagesList = (req, res) => {
     fetch('https://www.reddit.com/r/earthporn/top.json')
         .then(response => {
             response.json().then(data => {
-                const top = data.data.children[0].data.url;
+                const top = data.data.children[0];
                 const photoUrlOfTopPost = top.data.url;
                     res.status(200).json({
                         index: index,
                         earthPorn: photoUrlOfTopPost
                     })
+            }).catch(err => {
+                res.status(500).json({
+                    index: index,
+                    error: err
+                })
+                console.log(err);
             })
-        }).catch(e => {
-        console.log(e);
-        console.log("error happened");
+        }).catch(err => {
+        res.status(424).json({
+            index: index,
+            error: err
+        })
+        console.log(err);
     });
 };
