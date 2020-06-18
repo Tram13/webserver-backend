1c7600878dd7e861efea3a73dbbbb96a54d9a188
Tram13 <arno.vermote@hotmail.com>
2020-06-18 15:51:58 +0200

    try redirecting to image

--- controllers/ImageOfDayController.js
+++ controllers/ImageOfDayController.js
@@ -1,28 +1,42 @@
 const fetch = require("node-fetch");
+const index = "https://api.tram13.me";
+const ModesEnum = {"Image":0, "Json":1}
+Object.freeze(ModesEnum)
 
-exports.imagesList = (req, res) => {
-    const index = "https://api.tram13.me";
-    fetch('https://www.reddit.com/r/earthporn/top.json')
+//TODO: meerdere subreddits supporten
+
+function getTopImageFromSubreddit(res, subreddit, mode = ModesEnum.Image) {
+    const fetchUrl = "https://www.reddit.com/r/" + subreddit + "/top.json";
+    fetch(fetchUrl)
         .then(response => {
             response.json().then(data => {
-                const top = data.data.children[0];
-                const photoUrlOfTopPost = top.data.url;
+                const imageUrl = getTopOfRedditPost(data)
+                if (mode === ModesEnum.Image) {
+                    res.redirect(imageUrl);
+                }
+                else if (mode === ModesEnum.Json) {
                     res.status(200).json({
                         index: index,
-                        earthPorn: photoUrlOfTopPost
-                    })
+                        earthPorn: imageUrl
+                    });
+                }
             }).catch(err => {
-                res.status(500).json({
-                    index: index,
-                    error: err
-                })
                 console.log(err);
             })
         }).catch(err => {
-        res.status(424).json({
-            index: index,
-            error: err
-        })
         console.log(err);
     });
+}
+
+function getTopOfRedditPost(data) {
+    const top = data.data.children[0];
+    return top.data.url;
+}
+
+exports.imagesList = (req, res) => {
+    getTopImageFromSubreddit(res, "earthporn", ModesEnum.Json)
 };
+
+exports.earthPorn = (req, res) => {
+    getTopImageFromSubreddit(res, "earthporn", ModesEnum.Image)
+}
--- routes/imageOfDay.js
+++ routes/imageOfDay.js
@@ -5,4 +5,7 @@ const router = express.Router();
 // images of day
 router.get('/', imageOfDayController.imagesList);
 
+// r/earthporn
+router.get('/earthporn', imageOfDayController.earthPorn);
+
 module.exports = router;
