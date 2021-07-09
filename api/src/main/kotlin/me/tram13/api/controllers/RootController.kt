22376781663cab2daba8ca043d3f58133622552a
Tram13 <arno.vermote@hotmail.com>
2021-07-09 04:53:16 +0200

    Added root controller

---
+++ api/src/main/kotlin/me/tram13/api/controllers/RootController.kt
@@ -0,0 +1,41 @@
+package me.tram13.api.controllers
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.*
+import kotlin.collections.HashMap
+
+@RestController
+class RootController {
+
+    @Value("\${deployment-url}")
+    lateinit var apiUrl: String
+
+    private fun data(): HashMap<String, String> {
+        val content: HashMap<String, String> = HashMap()
+
+        val rootDirectory: Path = Paths.get("").toAbsolutePath()
+        val relativePath: Path = Path.of("src", "main", "kotlin", "me", "tram13", "api", "controllers")
+        val controllersDir = File(rootDirectory.toString(), relativePath.toString())
+        controllersDir.walk().forEach { file ->
+            if (file.isFile) {
+                val controller: String = file.nameWithoutExtension.dropLast(10).lowercase(Locale.getDefault())
+                val url = "$apiUrl/$controller"
+                content[controller] = url
+            }
+        }
+
+        // Rootcontroller heeft speciale waarde
+        content["root"] = apiUrl
+        return content
+    }
+
+    @GetMapping("/")
+    fun getContent(): HashMap<String, String> {
+        return data()
+    }
+}
