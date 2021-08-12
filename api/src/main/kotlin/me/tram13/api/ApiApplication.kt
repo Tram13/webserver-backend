bab7a0eb6ba30d7c0bb6a2d6dba41363e71df81c
Tram13 <arno.vermote@hotmail.com>
2021-08-12 12:56:42 +0200

    fixed issue with plugin

--- api/src/main/kotlin/me/tram13/api/ApiApplication.kt
+++ api/src/main/kotlin/me/tram13/api/ApiApplication.kt
@@ -4,7 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ApiApplication
+// TODO: check of 'open' nog steeds nodig is
+open class ApiApplication
 
 fun main(args: Array<String>) {
 	runApplication<ApiApplication>(*args)
