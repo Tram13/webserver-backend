9733e40ace4781fcb0d538308c5e104e5fa95592
Tram13 <arno.vermote@hotmail.com>
2021-08-17 13:13:45 +0200

    updated build

--- api/.run/deploy.sh
+++ api/.run/deploy.sh
@@ -1,7 +1,10 @@
-cd C:\Users\Arno\.m2\repository\me\tram13\api\0.0.1-SNAPSHOT || exit
 rename api-0.0.1-SNAPSHOT.jar spring.jar
 move spring.jar D:\Documents\Projects\webserver-backend\api\build\spring.jar
 del api-0.0.1-SNAPSHOT.pom
 del maven-metadata-local.xml
 del _remote.repositories
+cd D:\Documents\Projects\webserver-backend\api
+git add build/spring.jar
+git commit -m "updated build"
+git push
 ssh nas@tram13.me "sudo update webserver"
