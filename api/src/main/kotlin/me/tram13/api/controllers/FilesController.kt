e147769915b8f56d8c91e9fe9bf8740ac68e2880
Tram13 <arno.vermote@hotmail.com>
2023-02-22 04:08:38 +0100

    poging oneindig + 2

--- api/src/main/kotlin/me/tram13/api/controllers/FilesController.kt
+++ api/src/main/kotlin/me/tram13/api/controllers/FilesController.kt
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.io.File
+import java.net.MalformedURLException
 import java.nio.file.Path
 import java.nio.file.Paths
 
@@ -20,11 +21,21 @@ class FilesController {
         value = ["/lille_2023"],
         produces = [MediaType.APPLICATION_OCTET_STREAM_VALUE]
     )
-    fun getLilleZip(): ResponseEntity<File> {
-        val urlResource = UrlResource(downloadDirectory.resolve("lille_2023.zip").toString())
-        return ResponseEntity
-            .status(HttpStatus.OK)
-            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + urlResource.filename + "\"")
-            .body(urlResource.file)
+    fun getLilleZip(): ResponseEntity<out Any> {
+        val filePath = Paths.get(downloadDirectory.resolve("lille_2023.zip").toString())
+        return try {
+            val urlResource = UrlResource(filePath.toUri())
+            ResponseEntity
+                .status(HttpStatus.OK)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + urlResource.filename + "\"")
+                .body(urlResource)
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+            ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body("File not found on server: ${filePath.toUri()}")
+        }
+
+
     }
 }
