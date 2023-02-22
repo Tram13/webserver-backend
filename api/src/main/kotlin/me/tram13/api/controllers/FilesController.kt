4eb2aa686eee52f43b357a410597212d93598028
Tram13 <arno.vermote@hotmail.com>
2023-02-22 04:02:01 +0100

    poging oneindig + 1

--- api/src/main/kotlin/me/tram13/api/controllers/FilesController.kt
+++ api/src/main/kotlin/me/tram13/api/controllers/FilesController.kt
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
 
@@ -19,11 +20,11 @@ class FilesController {
         value = ["/lille_2023"],
         produces = [MediaType.APPLICATION_OCTET_STREAM_VALUE]
     )
-    fun getLilleZip(): ResponseEntity<UrlResource> {
+    fun getLilleZip(): ResponseEntity<File> {
         val urlResource = UrlResource(downloadDirectory.resolve("lille_2023.zip").toString())
         return ResponseEntity
             .status(HttpStatus.OK)
             .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + urlResource.filename + "\"")
-            .body(urlResource)
+            .body(urlResource.file)
     }
 }
