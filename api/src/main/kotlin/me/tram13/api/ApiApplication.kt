584052fa8bc88b2c03359461d07f497662953d59
Tram13 <arno.vermote@hotmail.com>
2023-02-22 03:09:47 +0100

    disabled postgres database for Spring Boot

--- api/src/main/kotlin/me/tram13/api/ApiApplication.kt
+++ api/src/main/kotlin/me/tram13/api/ApiApplication.kt
@@ -1,9 +1,10 @@
 package me.tram13.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 // TODO: check of 'open' nog steeds nodig is
 open class ApiApplication
 
