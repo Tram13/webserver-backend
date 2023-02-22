73c598ddc768e68ef07ed1ce47e99f01653b54a6
Tram13 <arno.vermote@hotmail.com>
2023-02-22 03:57:01 +0100

    4e poging

--- api/src/main/kotlin/me/tram13/api/controllers/FilesController.kt
+++ api/src/main/kotlin/me/tram13/api/controllers/FilesController.kt
@@ -1,12 +1,12 @@
 package me.tram13.api.controllers
 
+import org.springframework.core.io.UrlResource
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import java.io.InputStream
-import java.io.Serializable
 import java.nio.file.Path
 import java.nio.file.Paths
 
@@ -19,14 +19,11 @@ class FilesController {
         value = ["/lille_2023"],
         produces = [MediaType.APPLICATION_OCTET_STREAM_VALUE]
     )
-    fun getLilleZip(): ResponseEntity<out Serializable> {
-
-        val file: InputStream? = this::class.java.getResourceAsStream(downloadDirectory.resolve("lille_2023.zip").toString())
-        print(downloadDirectory.resolve("lille_2023.zip").toString())
-        return if (file != null) {
-            ResponseEntity.status(HttpStatus.OK).body(file.readBytes())
-        } else {
-            ResponseEntity.status(HttpStatus.NOT_FOUND).body("File not found on server")
-        }
+    fun getLilleZip(): ResponseEntity<UrlResource> {
+        val urlResource = UrlResource(downloadDirectory.resolve("lille_2023.zip").toString())
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + urlResource.filename + "\"")
+            .body(urlResource)
     }
 }
